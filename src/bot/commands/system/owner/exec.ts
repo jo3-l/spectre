1301af6894424af6ef36313b4212877872b00320
Joe <ravenclawjoe101325@gmail.com>
2019-12-14 19:24:49 -0800

    feat(exec): Add waiting message

--- src/bot/commands/system/owner/exec.ts
+++ src/bot/commands/system/owner/exec.ts
@@ -44,6 +44,7 @@ export default class ExecCommand extends Command {
 	}
 
 	public async exec(message: Message, { expr, timeout }: { expr: string; timeout: number }) {
+		await message.util!.send('⏱ Waiting for response...');
 		const timer = new Timer();
 		const result = await exec(expr, { timeout })
 			.catch(error => ({ stdout: null, stderr: error.stderr }));
