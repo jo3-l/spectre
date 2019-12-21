b4d9de1b05da80ad4ca77a31b60c265ca837a2ce
Joe <ravenclawjoe101325@gmail.com>
2019-12-21 12:54:32 -0800

    feat: edit-snipe command

---
+++ src/bot/listeners/discord/messageUpdate.ts
@@ -0,0 +1,32 @@
+import { Listener } from 'discord-akairo';
+import { Message, User } from 'discord.js';
+
+export default class MessageUpdateListener extends Listener {
+	public constructor() {
+		super('messageUpdate', {
+			emitter: 'client',
+			event: 'messageUpdate',
+			category: 'Discord',
+		});
+	}
+
+	public exec(oldMessage: Message, newMessage: Message) {
+		if (oldMessage.content === newMessage.content || !oldMessage.content) return;
+		newMessage.channel.editSnipe = {
+			author: newMessage.author,
+			timestamp: newMessage.editedAt!,
+			content: oldMessage.content,
+		};
+	}
+}
+
+declare module 'discord.js' {
+	interface TextChannel { editSnipe: EditSnipe }
+	interface DMChannel { editSnipe: EditSnipe }
+}
+
+interface EditSnipe {
+	author: User;
+	timestamp: Date;
+	content: string;
+}
