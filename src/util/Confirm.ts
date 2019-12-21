662769d1c7101f520e5c9ace1617a9f72ecc47d9
Joe <ravenclawjoe101325@gmail.com>
2019-12-21 12:54:21 -0800

    feat: Confirmation util

---
+++ src/util/Confirm.ts
@@ -0,0 +1,32 @@
+import { Message, MessageEmbed } from 'discord.js';
+
+const RESPONSES = {
+	YES: ['yes', 'y', 'yup'],
+	NO: ['no', 'n', 'nah', 'nope'],
+	CANCEL: ['cancel'],
+};
+
+export enum Responses {
+	Yes = 0, No, Canceled, Timeout,
+}
+
+export default class Confirmation {
+	public constructor(public readonly message: Message, public readonly embed: MessageEmbed, public readonly timeout = 15000) { }
+	public async run() {
+		const filter = (m: Message) => m.author.id === this.message.author.id && Object.values(RESPONSES).flat().includes(m.content);
+		const message = await this.message.channel.send(this.embed);
+		let response: Message;
+		try {
+			response = (await message.channel.awaitMessages(filter, {
+				max: 1,
+				time: this.timeout,
+				errors: ['time'],
+			})).first()!;
+		} catch { return Responses.Timeout; }
+
+		const content = response.content.toLowerCase();
+		if (RESPONSES.YES.includes(content)) return Responses.Yes;
+		if (RESPONSES.CANCEL.includes(content)) return Responses.Canceled;
+		return Responses.No;
+	}
+}
