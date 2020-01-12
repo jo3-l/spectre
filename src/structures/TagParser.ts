1007b314a4b92f23b5027a04597d1be7f40a298a
Joe <ravenclawjoe101325@gmail.com>
2020-01-11 22:19:32 -0800

    Allow tagparser to work with arbitrary templates

--- src/structures/TagParser.ts
+++ src/structures/TagParser.ts
@@ -1,44 +1,17 @@
-import { User, Guild, GuildMember, TextChannel, MessageEmbed } from 'discord.js';
-import { ordinal } from '../util/Util';
+import SpectreEmbed from './SpectreEmbed';
 
 enum EmbedParseErrors {
 	Invalid = 0, NoContent
 }
 
+
+interface Templates {
+	[templateName: string]: string | number | Record<string, string | number>;
+}
+
 function isObject(obj: any) {
 	const type = typeof obj;
 	return (type === 'function' || type === 'object') && Boolean(obj);
-}
-
-export function createTemplates({ user, guild, member, level, image, channel }: Context): Templates {
-	return {
-		user: {
-			id: user.id,
-			discrim: user.discriminator,
-			tag: user.tag,
-			mention: user.toString(),
-			username: user.username,
-			avatar: user.displayAvatarURL(),
-		},
-		member: {
-			nickname: member.displayName,
-		},
-		guild: {
-			icon: guild.iconURL() || '',
-			name: guild.name,
-			id: guild.id,
-			membercount: guild.memberCount.toString(),
-			membercountordinal: ordinal(guild.memberCount),
-		},
-		channel: {
-			name: channel.name,
-			mention: channel.toString(),
-		},
-		level,
-		image,
-		rb: '{',
-		lb: '}',
-	};
 }
 
 export function parseOne(template: string, templates: Templates): string | undefined {
@@ -50,25 +23,27 @@ export function parseOne(template: string, templates: Templates): string | undef
 	const data = templates[struct as keyof Templates];
 	if (!data) return;
 	if (isObject(data) && prop in (data as object)) {
-		return data[prop as keyof typeof data];
+		return (data as Record<string, string | number>)[prop].toString();
 	}
 
 	return data.toString();
 }
 
 export function parse(expr: string, templates: Templates) {
+	templates.rb = '}';
+	templates.lb = '{';
 	return expr.replace(
 		/({{(.*?)}})/g,
 		(_, all: string, expression: string) => parseOne(expression, templates) ?? all,
 	);
 }
 
-export function parseEmbed(resolvable: string, templates: Templates): MessageEmbed | EmbedParseErrors {
+export function parseEmbed(resolvable: string, templates: Templates): SpectreEmbed | EmbedParseErrors {
 	const ignoredParts = ['timestamp', 'type', 'video', 'provider', 'length', 'hexColor', 'createdAt', 'color'];
-	let embed: MessageEmbed;
+	let embed: SpectreEmbed;
 	try {
 		const parsed = JSON.parse(resolvable);
-		embed = new MessageEmbed(parsed);
+		embed = new SpectreEmbed(parsed);
 	} catch {
 		return EmbedParseErrors.Invalid;
 	}
@@ -91,40 +66,4 @@ export function parseEmbed(resolvable: string, templates: Templates): MessageEmb
 		}
 	}
 	return embed.length ? embed : EmbedParseErrors.NoContent;
-}
-
-interface Context {
-	user: User;
-	guild: Guild;
-	member: GuildMember;
-	channel: TextChannel;
-	level?: number;
-	image?: string;
-}
-
-interface Templates {
-	user: {
-		id: string;
-		discrim: string;
-		tag: string;
-		mention: string;
-		username: string;
-		avatar: string;
-	};
-	member: { nickname: string };
-	channel: {
-		name: string;
-		mention: string;
-	};
-	guild: {
-		icon: string;
-		name: string;
-		id: string;
-		membercount: string;
-		membercountordinal: string;
-	};
-	level?: number;
-	image?: string;
-	rb: '{';
-	lb: '}';
 }
