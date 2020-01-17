5bf4ac8745855daa8e225e342cd826b4083313b4
Joe <ravenclawjoe101325@gmail.com>
2020-01-16 20:06:21 -0800

    chore: comment rank card code for maintanability

--- src/commands/levels/rank.ts
+++ src/commands/levels/rank.ts
@@ -84,39 +84,39 @@ export default class RankCommand extends Command {
 
 		canvas
 			.addCircularImage(avatar, 141, 141, 80)
-			.setColor('#000000')
+			.setColor('#000000') // Create a black outline for the presence color circle
 			.addCircle(184, 194, 24)
-			.setColor(statusColors[user.presence.status])
+			.setColor(statusColors[user.presence.status]) // Create presence color circle
 			.addCircle(184, 194, 20)
 			.setColor(`#${color}`)
 			.setTextFont(fonts.centuryGothic)
 			.setTextAlign('right')
-			.addText(level, 870, 100)
-			.measureText(level, size => correctX = 865 - size.width)
+			.addText(level, 870, 100) // Add the numeric level
+			.measureText(level, size => correctX = 865 - size.width) // Adjust correctX appropriately
 			.setTextFont(fonts.lucidaSans)
-			.addText('LEVEL', correctX, 100)
-			.measureText('LEVEL', size => correctX -= size.width + 20)
+			.addText('LEVEL', correctX, 100) // Add leading "LEVEL" before the actual numeric level
+			.measureText('LEVEL', size => correctX -= size.width + 20) // Adjust correctX appropriately
 			.setTextFont(fonts.centuryGothic)
 			.setColor('#FFFFFF')
-			.addText(`#${format(Number(rank))}`, correctX, 100)
-			.measureText(`#${format(Number(rank))}`, size => correctX -= size.width + 5)
+			.addText(`#${format(Number(rank))}`, correctX, 100) // Add the numeric rank
+			.measureText(`#${format(Number(rank))}`, size => correctX -= size.width + 5) // Adjust correctX appropriately
 			.setTextFont(fonts.lucidaSans)
-			.addText('RANK', correctX, 100)
+			.addText('RANK', correctX, 100) // Add leading "RANK" before numeric rank
 			.setTextSize(38)
 			.setTextAlign('start')
-			.addResponsiveText(user.username, 260, 160, 322)
-			.measureText(user.username, ({ width }: TextMetrics) => correctX = width)
+			.addResponsiveText(user.username, 260, 160, 322) // Add username
+			.measureText(user.username, ({ width }: TextMetrics) => correctX = width) // Adjust correctX
 			.setTextFont(fonts.lucidaSans)
 			.setColor('#7F8384')
-			.addText(`#${user.discriminator}`, correctX + 265, 160)
+			.addText(`#${user.discriminator}`, correctX + 265, 160) // Add trailing discrim after username
 			.setTextSize(24)
 			.setColor('#7F8384')
 			.setTextAlign('right')
-			.addText(`/ ${format(Number(total))} XP`, 850, 160)
+			.addText(`/ ${format(Number(total))} XP`, 850, 160) // Add numeric XP total for level at top right above progress bar
 			.measureText(`/ ${format(Number(total))} XP`, (size: TextMetrics) => correctX = 845 - size.width)
 			.setColor('#FFFFFF')
-			.addText(format(Number(current)).toString(), correctX, 160)
-			.setColor('#484B4E')
+			.addText(format(Number(current)).toString(), correctX, 160) // Add the actual current level of user
+			.setColor('#484B4E') // Create rounded rectangle
 			.beginPath()
 			.arc(270, 200, 20, Math.PI * 0.5, Math.PI * 1.5)
 			.fill()
@@ -128,13 +128,15 @@ export default class RankCommand extends Command {
 			.closePath()
 			.addCircle(270, 200, 20);
 
+		// Calculate the percentage
 		let percent = toPercentage(Number(current), Number(total));
+		// Return the constructed canvas if percentage under 40 (radius of a single rounded corner of the rounded progress bar)
 		if (percent < 40) return canvas.toBufferAsync();
 		canvas
 			.setColor(`#${color}`)
-			.addCircle(270, 200, 20)
-			.addRect(270, 180, percent -= 40, 40)
-			.addCircle(270 + percent, 200, 20);
+			.addCircle(270, 200, 20) // Add the circle on the left
+			.addRect(270, 180, percent -= 40, 40) // Add progress
+			.addCircle(270 + percent, 200, 20); // Add the circle on the right
 
 		return canvas.toBufferAsync();
 	}
